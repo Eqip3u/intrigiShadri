7e2046f804a12ec82ca3fb80775ccde13bf126f8
Eqip3u <Eqip3u.dem@yandex.ru>
2016-12-15 20:59:56 +0500

    get/post add

--- app/models/note.js
+++ app/models/note.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var NoteSchema = new Schema({
-  title: String,
-  name: String
+  title: String
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var mongoose = require('mongoose');
+var Note = require('./app/models/note');
 
 var app = express();
 
@@ -12,10 +13,38 @@ mongoose.connect('mongodb://Admin:LgwfAWz2@ds135798.mlab.com:35798/intrigi');
 
 var port = process.env.PORT || 8080;
 
+router.use(function(req, res, next) {
+  console.log('xorowo!');
+  next();
+});
 
 router.get('/', function(req, res) {
   res.json({message: 'privet'});
 });
+
+router.route('/notes')
+  .post(function(req, res){
+    var note = new Note();
+    note.title = req.body.title;
+
+    note.save(function(err){
+      if(err)
+        res.send(err);
+      
+      res.json({message: 'Заметка добавлена!'});
+    });
+  })
+
+  .get(function(req, res){
+    Note.find(function(err, notes){
+      if (err)
+        res.send(err);
+
+      res.json(notes);
+    });
+  });
+
+
 
 app.use('/api', router);
 
